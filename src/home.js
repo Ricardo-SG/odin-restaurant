28523e86e7aed6977e317b6fac9f8efc891a4706
Ricardo-SG <soriano.gomez.ricardo@gmail.com>
2022-10-20 16:32:31 +0200

    moved code related to homepage to a second js module

---
+++ src/home.js
@@ -0,0 +1,51 @@
+import Croquetas from './images/croquetas.jpg'; // The croquette (most important element of this web)
+/* This code under here will probably be exported into a module */
+/* We're gonna build the home canvas now */
+function loadHome (fatherContainer) {
+
+    /* 1) we define every html element we're gonna put into the canvas */
+    const homeCanvas   = document.createElement('div');
+    const homeTitleDiv = document.createElement('div');
+    const pTitle1      = document.createElement('p');
+    const pTitle2      = document.createElement('p');
+    const separation   = document.createElement('hr');
+    const pText        = document.createElement('p');
+    const myCroqueta   = document.createElement('img');
+    const btnMenu      = document.createElement('button'); 
+    
+
+    /* 2) we put their classes */
+    homeCanvas.className = 'inner-canvas';
+    pTitle1.className    = 'big-text';
+    pTitle2.className    = 'smol-text';
+    pText.className      = 'normie-text';
+    myCroqueta.className = 'croqueta';
+    btnMenu.className    = 'button-for-menu';
+    
+    
+
+    /* 3) we put their inner text */
+    pTitle1.innerText = 'Welcome to'; 
+    pTitle2.innerText = 'your granny\'s kitchen';
+    pText.innerText   = 'Enjoy the cooking only your granny can cook';
+    myCroqueta.src    = Croquetas; // not a text but a source 
+    btnMenu.innerText = 'MENU';
+    
+
+    /* 4) we put their functions */
+    //btnMenu.onclick = loadMenu;
+
+    /* 5) But it's the pelvic thrust */
+    homeTitleDiv.append(pTitle1,pTitle2,separation,pText,myCroqueta,btnMenu);
+
+    /* 6) that really drives you insane */
+    homeCanvas.appendChild(homeTitleDiv);
+    
+    /* 7) let's do the time warp again!! */
+    fatherContainer.appendChild(homeCanvas);
+};
+
+
+
+
+export default {loadHome};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 /* Import section --------------------------------------------------- */
 import './styles.scss'; // SCSS styles for our web
-import Croquetas from './images/croquetas.jpg'; // The croquette (most important element of this web)
+import HomePage from './home.js';
 /* ------------------------------------------------------------------ */
 /* We retrieve the container, that will hold all our web */
 const container = document.getElementById('container');
@@ -44,69 +44,24 @@ footer.className ='footer';
 footer.innerText = 'Designed by Ric for The Odin Project';
 container.appendChild(footer);
 
-loadHome();
-/* This code under here will probably be exported into a module */
-/* We're gonna build the home canvas now */
-function loadHome () {
-
-    /* 1) we clean the canvas */
-    cleanCanvas();
-
-    /* 2) we define every html element we're gonna put into the canvas */
-    const homeCanvas   = document.createElement('div');
-    const homeTitleDiv = document.createElement('div');
-    const pTitle1      = document.createElement('p');
-    const pTitle2      = document.createElement('p');
-    const separation   = document.createElement('hr');
-    const pText        = document.createElement('p');
-    const myCroqueta   = document.createElement('img');
-    const btnMenu      = document.createElement('button'); 
-    
-
-    /* 3) we put their classes */
-    homeCanvas.className = 'inner-canvas';
-    pTitle1.className    = 'big-text';
-    pTitle2.className    = 'smol-text';
-    pText.className      = 'normie-text';
-    myCroqueta.className = 'croqueta';
-    btnMenu.className    = 'button-for-menu';
-    
-    
-
-    /* 4) we put their inner text */
-    pTitle1.innerText = 'Welcome to'; 
-    pTitle2.innerText = 'your granny\'s kitchen';
-    pText.innerText   = 'Enjoy the cooking only your granny can cook';
-    myCroqueta.src    = Croquetas; // not a text but a source 
-    btnMenu.innerText = 'MENU';
-    
-
-    /* 5) we put their functions */
-    btnMenu.onclick = loadMenu;
-
-    /* 6) But it's the pelvic thrust */
-    homeTitleDiv.append(pTitle1,pTitle2,separation,pText,myCroqueta,btnMenu);
-
-    /* 7) that really drives you insane */
-    homeCanvas.appendChild(homeTitleDiv);
-    
-    /* 8) let's do the time warp again!! */
-    canvas.appendChild(homeCanvas);
-};
+HomePage.loadHome(canvas);
 
 
-function loadMenu () {
-    console.log('yippi ka yei hijos de puta');
-};
-/*
-function loadContact () {
-
-};
-*/
-
+// Index.js functions
 function cleanCanvas () {
 
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
 }
+
+function loadHome() {
+    console.log('ola k ase');
+    console.log(canvas);
+    cleanCanvas();
+    HomePage.loadHome(canvas);
+}
+
+function loadMenu () {
+    console.log('yippi ka yei hijos de puta');
+};
